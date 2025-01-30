482008663fe96890bbfb6bd6ee8d6d1df4a32625
xavier bahegne <xavierbahegne@gmail.com>
2025-01-30 11:37:53 +0100

    .

--- app/layout.tsx
+++ app/layout.tsx
@@ -66,7 +66,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="fr">
       <body
         className={`${workSans.variable}`}
       >
